9b1e36c2b10e6ad7444a57dbf61889eb47d9dac9
Isaac Santiago <isaac.pereira@zumpy.com.br>
2024-02-09 12:57:15 -0300

    feat: handle spring authentication with keycloak

---
+++ src/main/java/org/eoisaac/springkeycloakauth/SpringKeycloakAuthApplication.java
@@ -0,0 +1,13 @@
+package org.eoisaac.springkeycloakauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeycloakAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeycloakAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eoisaac/springkeycloakauth/SpringKeycloakAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eoisaac.springkeycloakauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
